3e4c3650170df44d5f45b99d9081d1dd3b6e48fa
XDGFX <callum.morrison@mac.com>
2020-08-23 22:30:05 +0100

    Implementing o-auth for spotify

--- ultrasonics_api/__init__.py
+++ ultrasonics_api/__init__.py
@@ -1,6 +1,8 @@
 import os
 
 from flask import Flask
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
 
 from . import ultrasonics_api
 
@@ -8,9 +10,14 @@ from . import ultrasonics_api
 def create_app():
     app = Flask(__name__)
     app.config.from_mapping(
-        SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev_key'
+        SECRET_KEY=os.environ.get('SECRET_KEY') or 'dev_key'
+    )
+
+    limiter = Limiter(
+        app,
+        key_func=get_remote_address,
+        default_limits=["200 per day", "50 per hour"]
     )
 
     app.register_blueprint(ultrasonics_api.bp)
-
     return app
--- ultrasonics_api/ultrasonics_api.py
+++ ultrasonics_api/ultrasonics_api.py
@@ -3,16 +3,19 @@
 """
 ultrasonics_api
 
-Used as a proxy server to forward API requests to various services which require private API keys.
+Used as a proxy server to forward api requests to various services which require private api keys.
+
+All api requests should be made to /api/<service>/<subpath>
+where <subpath> is the same as sending directly to the respective api.
 
 XDGFX, 2020
 """
 
 import os
-
-from flask import Blueprint, jsonify, request
+from urllib.parse import urlencode
 
 import requests
+from flask import Blueprint, Response, jsonify, redirect, request
 
 bp = Blueprint('ultrasonics_api', __name__)
 
@@ -20,13 +23,18 @@ bp = Blueprint('ultrasonics_api', __name__)
 def index():
     return jsonify({
         "name": "ultrasonics_api",
-        "api_version": "v1"
+        "supported apis": [
+            "spotify"
+        ]
     })
 
-@bp.route('/api/v1/spotify/<path:subpath>', methods=["GET", "POST", "PUT", "DELETE"])
-def api_v1_spotify(subpath):
+class Spotify():
+    valid_states = []
+
+@bp.route('/api/spotify/<path:subpath>', methods=["GET", "POST", "PUT", "DELETE"])
+def api_spotify(subpath):
     """
-    Spotify API proxy. Adds a 
+    Spotify api proxy. Adds an app client secret key to all requests.
     """
     base_url = "https://api.spotify.com/"
     url = base_url + subpath
@@ -43,3 +51,40 @@ def api_v1_spotify(subpath):
         r = requests.delete(url = url, params = params)
 
     return r.json()
+
+@bp.route('/api/spotify_auth_request')
+def api_spotify_auth_request():
+    """
+    Spotify api proxy specifically for the app auth endpoint.
+    """
+    from uuid import uuid4
+
+    base_url = "https://accounts.spotify.com/authorize/?"
+
+    params = {
+        "client_id": os.environ.get('SPOTIFY_CLIENT_ID'),
+        "response_type": "code",
+        "redirect_uri": "https://ultrasonics-api.herokuapp.com/api/spotify_auth",
+        "state": uuid4()
+    }
+
+    Spotify.valid_states.append(params["state"])
+
+    url = base_url + urlencode(params)
+
+    return redirect(url, 302)
+
+
+@bp.route('/api/spotify_auth')
+def api_spotify_auth():
+    code = request.args.get("code", None)
+    error = request.args.get("error", None)
+    state = request.args.get("state")
+
+    if state not in Spotify.valid_states:
+        return "State returned was not valid", 500
+
+    if error:
+        return error
+    else:
+        return code
